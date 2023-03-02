ebfcf2e2a98f18c56040117f42c1d9a75d0c9daf
Novi Patricia <novi.patricia@gmail.com>
2023-03-02 08:49:27 +0700

    update configs

--- archs/__init__.py
+++ archs/__init__.py
@@ -9,7 +9,7 @@ __all__ = ['build_network']
 
 arch_folder = os.path.dirname(os.path.abspath(__file__))
 arch_filenames = [os.path.splitext(os.path.basename(f))[0] for f in os.listdir(arch_folder) if f.endswith('_arch.py')]
-# _arch_modules = [importlib.import_module(f'archs.{file_name}') for file_name in arch_filenames]
+_arch_modules = [importlib.import_module(f'archs.{file_name}') for file_name in arch_filenames]
 
 
 def build_network(opt):
